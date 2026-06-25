8c17d21e5e8507485bb12c721f3934c9e39d8c33
Ulises Ramirez <uramirez@pentafon.com>
2026-06-25 11:12:15 -0600

    Ahora si con certificados VIVA

--- src/server.js
+++ src/server.js
@@ -7,19 +7,12 @@ const app = require('./app');
 const PORT = 3001;
 
 
-// para servidor en prod
- // const certBasePath = path.join(__dirname, '../certificado');
-
-//const options = {
-  //key: fs.readFileSync(path.join(certBasePath, 'STAR.pentafon.com_key.txt')),
-  //cert: fs.readFileSync(path.join(certBasePath, 'STAR_pentafon_com.crt')),
-  //ca: fs.readFileSync(path.join(certBasePath, 'STAR_pentafon_com.ca-bundle'))
-//};
+const certBasePath = path.join(__dirname, '../certificado');
 
 const options = {
-  key: fs.readFileSync('/home/uramirez/tablero-genesys/certificado/STAR.pentafon.com_key.txt'),
-  cert: fs.readFileSync('/home/uramirez/tablero-genesys/certificado/STAR_pentafon_com.crt'),
-  ca: fs.readFileSync('/home/uramirez/tablero-genesys/certificado/STAR_pentafon_com.ca-bundle')
+  key: fs.readFileSync(path.join(certBasePath, 'STAR.pentafon.com_key.txt')),
+  cert: fs.readFileSync(path.join(certBasePath, 'STAR_pentafon_com.crt')),
+  ca: fs.readFileSync(path.join(certBasePath, 'STAR_pentafon_com.ca-bundle'))
 };
 
 https.createServer(options, app).listen(PORT, () => {
